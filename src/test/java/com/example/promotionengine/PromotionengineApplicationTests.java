6f98548edeefd74a61084c5866fa6b088e90c343
singhalshubham89@gmail.com <singhalshubham89@gmail.com>
2020-12-17 23:45:55 +0530

    Check For Multiple Promotion Test case

--- src/test/java/com/example/promotionengine/PromotionengineApplicationTests.java
+++ src/test/java/com/example/promotionengine/PromotionengineApplicationTests.java
@@ -100,8 +100,8 @@ class PromotionengineApplicationTests {
         A + B => 60
 
         Scenario A
-        2 * A 50
-        3 * B 30
+        2 * A + 2*B 120
+        1 * B 30
         1 * C 20
 
         total = 170
@@ -117,5 +117,69 @@ class PromotionengineApplicationTests {
         promotions.add(new Buy2ItemPromotion(items.get('A'), items.get('B'), 60));
 
         assert (cartService.getCartPriceWithPromotion(cart, itemPrice, promotions) == 170);
+    }
+
+    @Test
+    void cartWithMixPromotionsHavingOnlyBuyNPromotionEligibile(){
+        /*
+       Active Promotions
+        3 of A's for 130
+        2 of B's for 45
+        C & D for 30
+
+
+        Scenario B
+        5 * A 130 + 2*50
+        5 * B 45 + 45 + 30
+        1 * C 20
+        Total 370
+
+         */
+
+        Map<Item, Integer> itemsWithQuantity = new HashMap<>();
+        itemsWithQuantity.put(items.get('A'), 5);
+        itemsWithQuantity.put(items.get('B'), 5);
+        itemsWithQuantity.put(items.get('C'), 1);
+        Cart cart = new Cart(itemsWithQuantity);
+
+        ArrayList<Promotion> promotions = new ArrayList<>();
+        promotions.add(new BuyNPromotionService(items.get('A'), 3, 130));
+        promotions.add(new BuyNPromotionService(items.get('B'), 2, 45));
+        promotions.add(new Buy2ItemPromotion(items.get('C'), items.get('D'), 30));
+
+        assert (cartService.getCartPriceWithPromotion(cart, itemPrice, promotions) == 370);
+    }
+
+    @Test
+    void cartWithMixPromotionsHavingMultiplePromotionEligibile(){
+        /*
+       Active Promotions
+        3 of A's for 130
+        2 of B's for 45
+        C & D for 30
+
+
+        Scenario B
+        3 * A 130
+        5 * B 45 + 45 + 30
+        1 * C
+        1 * D 30
+        Total 280
+
+         */
+
+        Map<Item, Integer> itemsWithQuantity = new HashMap<>();
+        itemsWithQuantity.put(items.get('A'), 3);
+        itemsWithQuantity.put(items.get('B'), 5);
+        itemsWithQuantity.put(items.get('C'), 1);
+        itemsWithQuantity.put(items.get('D'), 1);
+        Cart cart = new Cart(itemsWithQuantity);
+
+        ArrayList<Promotion> promotions = new ArrayList<>();
+        promotions.add(new BuyNPromotionService(items.get('A'), 3, 130));
+        promotions.add(new BuyNPromotionService(items.get('B'), 2, 45));
+        promotions.add(new Buy2ItemPromotion(items.get('C'), items.get('D'), 30));
+
+        assert (cartService.getCartPriceWithPromotion(cart, itemPrice, promotions) == 280);
     }
 }
